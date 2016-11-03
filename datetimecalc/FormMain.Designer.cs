bb2a11432d84a973db132c7dfd600b316a2a310a
Yuki Nishina <ynishina@live.com>
2016-11-03 23:11:17 +0900

    初回コミット

---
+++ datetimecalc/FormMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace datetimecalc
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.numericUpDownDay = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 19);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Location = new System.Drawing.Point(171, 39);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(33, 12);
+            this.labelDay.TabIndex = 1;
+            this.labelDay.Text = "日(&D)";
+            // 
+            // numericUpDownDay
+            // 
+            this.numericUpDownDay.Location = new System.Drawing.Point(84, 37);
+            this.numericUpDownDay.Name = "numericUpDownDay";
+            this.numericUpDownDay.Size = new System.Drawing.Size(81, 19);
+            this.numericUpDownDay.TabIndex = 2;
+            this.numericUpDownDay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownDay.ValueChanged += new System.EventHandler(this.numericUpDownDay_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(156, 62);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(120, 19);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 93);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.numericUpDownDay);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Datetime Calc";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.NumericUpDown numericUpDownDay;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
+
---
+++ datetimecalc/FormMain.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace datetimecalc
+{
+    public partial class FormMain : Form
+    {
+        void calcDays()
+        {
+            numericUpDownDay.Value = (dateTimePicker2.Value - dateTimePicker1.Value).Days;
+        }
+
+        void showErrorMessage(Exception exception)
+        {
+            MessageBox.Show(exception.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            dateTimePicker1.MinDate = DateTimePicker.MinimumDateTime;
+            dateTimePicker1.MaxDate = DateTimePicker.MaximumDateTime;
+            dateTimePicker2.MinDate = DateTimePicker.MinimumDateTime;
+            dateTimePicker2.MaxDate = DateTimePicker.MaximumDateTime;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            dateTimePicker1.Value = dateTimePicker1.Value.Date;
+            dateTimePicker2.Value = dateTimePicker2.Value.Date;
+            dateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownDay.ValueChanged -= numericUpDownDay_ValueChanged;
+
+            try
+            {
+                numericUpDownDay.Maximum = (dateTimePicker1.MaxDate - dateTimePicker1.Value.Date).Days;
+                numericUpDownDay.Minimum = ((dateTimePicker1.Value.Date - dateTimePicker1.MinDate).Days * -1);
+                calcDays();
+            }
+            catch (Exception exception)
+            {
+                showErrorMessage(exception);
+            }
+            finally
+            {
+                numericUpDownDay.ValueChanged += numericUpDownDay_ValueChanged;
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDownDay.ValueChanged -= numericUpDownDay_ValueChanged;
+
+            try
+            {
+                calcDays();
+            }
+            catch (Exception exception)
+            {
+                showErrorMessage(exception);
+            }
+            finally
+            {
+                numericUpDownDay.ValueChanged += numericUpDownDay_ValueChanged;
+            }
+        }
+
+        private void numericUpDownDay_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.ValueChanged -= dateTimePicker2_ValueChanged;
+
+            try
+            {
+                dateTimePicker2.Value = dateTimePicker1.Value.AddDays((double)numericUpDownDay.Value);
+            }
+            catch (Exception exception)
+            {
+                showErrorMessage(exception);
+            }
+            finally
+            {
+                dateTimePicker2.ValueChanged += dateTimePicker2_ValueChanged;
+            }
+        }
+    }
+}
